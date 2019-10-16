dcb1668017a3bf32efa52531735d16eefde86f98
x94jp6rmp <x94jp6rmp@gmail.com>
2019-10-16 23:11:21 +0800

    merge to final version

---
+++ lesson/lesson1.js
@@ -0,0 +1,111 @@
+
+
+function start(e) {
+	VF = Vex.Flow;
+	width = document.body.clientWidth;
+	height = document.body.clientHeight;
+
+	var div = document.getElementById("boo");
+	var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+	$("svg")[0].id="svgRenderer";
+	renderer.resize(width, height*0.3);
+	var context = renderer.getContext();
+
+	var tickContext = new VF.TickContext();
+	var stave = new VF.Stave(width*0.1, height*0.05, width*0.8)
+	.addClef('treble')
+	.addTimeSignature("4/4");
+
+	var stave2 = new VF.Stave(width*0.1, height*0.15, width*0.8)
+	.addClef('bass')
+	.addTimeSignature("4/4");
+
+	stave.setContext(context).draw();
+	// console.log($("rect").x)
+	// stave.drawVertical($("rect").x);
+	stave2.setContext(context).draw();
+	
+	//大括弧 
+	var connector = new VF.StaveConnector(stave,stave2);
+	var line = new VF.StaveConnector(stave, stave2);
+	connector.setType(VF.StaveConnector.type.BRACE);
+	connector.setContext(context);
+	line.setType(VF.StaveConnector.type.SINGLE);
+	line.setContext(context);
+	connector.draw();
+	line.draw();
+
+
+	
+	document.getElementById('start').addEventListener('click',(e)=>{
+	//c4的int_value為60
+		var notes = [
+			['c', '', '4', '4'],
+			['d', '', '4', '4'],
+			['c', '', '4', '4'],
+			['d', '', '4', '4'],
+			['c', '', '4', '4'],
+			['d', '', '4', '4'],
+			['c', '', '4', '1']
+		].map(([letter, acc, octave, durations]) => {
+			const note = new VF.StaveNote({
+				clef: 'treble',
+				keys: [`${letter}${acc}/${octave}`],
+				duration: `${durations}`,
+			})
+			.setContext(context)
+			.setStave(stave);
+			if(acc) note.addAccidental(0, new VF.Accidental(acc));
+			tickContext.addTickable(note);
+
+			return note;
+		});
+		tickContext.preFormat().setX(width*0.7);
+		const visibleNoteGroups = [];
+		setInterval(() => {
+			note = notes.shift();
+			if(!note) return;
+			const group = context.openGroup();
+			visibleNoteGroups.push(group);
+			note.draw();
+			let note_temp=this.note;
+			let keys = note_temp.keys[0];
+			context.closeGroup();
+			group.classList.add('scroll');
+			const box =group.getBoundingClientRect();
+			group.classList.add('scrolling');
+			group.style.setProperty("--time",(width*0.7)/80+"s"); 
+
+			window.setTimeout(() => {
+				let pitch = document.getElementById("tone").value;
+				var id = note_temp.attrs.id;
+				console.log(keys[0])
+				console.log(pitch[0]);
+				if(pitch[0]==keys[0]){
+					console.log("same!!!" + id);
+					$("g #vf-"+id+" path").attr("fill","#12ca3a");
+					$("g #vf-"+id+" path").attr("stroke","#12ca3a");
+				}
+				else{
+					console.log("different!!!" + id);
+					$("g #vf-"+id+" path").attr("fill","red");
+					$("g #vf-"+id+" path").attr("stroke","red");
+				}
+				const index = visibleNoteGroups.indexOf(group);
+				if(index === -1) return;
+				group.classList.add('too-slow');
+				visibleNoteGroups.shift();
+			}, (width*0.7)/80*1000);
+		}, 1500);//控制音符之間距離
+	})
+}
+
+/*
+https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+
+document.addEventListener(event, function, useCapture)
+
+true - The event handler is executed in the capturing phase
+false- Default. The event handler is executed in the bubbling phase
+*/
+window.addEventListener( "load", start, false );
--- piano.js
+++ piano.js
@@ -1,42 +1,41 @@
 /*
   https://codepen.io/anon/pen/YvmZYE
 */
-//soundpack定義一包一包的資料，給audio用，包含number：音符號碼跟url：音檔來源
-var soundpack=[];
-//推一包一包的資料進去，這邊會提供audio做渲染
-var soundpack_index = []; 
+var soundpack=[];//soundpack定義一包一包的資料，給audio用，包含number：音符號碼跟url：音檔來源
+
+var soundpack_index = []; //要載入的音符號碼陣列
 
 var soundpack_index_mapping=[];
 
 soundpack_index_mapping["1"]    = 1;
-soundpack_index_mapping["1.5"]  = 2;
-soundpack_index_mapping["2"]    = 3;
+soundpack_index_mapping["1.5"]  = 1.5;
+soundpack_index_mapping["2"]    = 2;
 
-soundpack_index_mapping["3"]    = 4;
-soundpack_index_mapping["3.5"]  = 5;
-soundpack_index_mapping["4"]    = 6;
-soundpack_index_mapping["4.5"]  = 7;
-soundpack_index_mapping["5"]    = 8;
-soundpack_index_mapping["6"]    = 9;
-soundpack_index_mapping["6.5"]  = 10;
-soundpack_index_mapping["7"]    = 11;
-soundpack_index_mapping["7.5"]  = 12;
-soundpack_index_mapping["8"]    = 13;
-soundpack_index_mapping["8.5"]  = 14;
-soundpack_index_mapping["9"]    = 15;
+soundpack_index_mapping["3"]    = 3;
+soundpack_index_mapping["3.5"]  = 3.5;
+soundpack_index_mapping["4"]    = 4;
+soundpack_index_mapping["4.5"]  = 4.5;
+soundpack_index_mapping["5"]    = 5;
+soundpack_index_mapping["6"]    = 6;
+soundpack_index_mapping["6.5"]  = 6.5;
+soundpack_index_mapping["7"]    = 7;
+soundpack_index_mapping["7.5"]  = 7.5;
+soundpack_index_mapping["8"]    = 8;
+soundpack_index_mapping["8.5"]  = 8.5;
+soundpack_index_mapping["9"]    = 9;
 
-soundpack_index_mapping["10"]    = 16;
-soundpack_index_mapping["10.5"]  = 17;
-soundpack_index_mapping["11"]    = 18;
-soundpack_index_mapping["11.5"]  = 19;
-soundpack_index_mapping["12"]    = 20;
-soundpack_index_mapping["13"]    = 21;
-soundpack_index_mapping["13.5"]  = 22;
-soundpack_index_mapping["14"]    = 23;
-soundpack_index_mapping["14.5"]  = 24;
-soundpack_index_mapping["15"]    = 25;
-soundpack_index_mapping["15.5"]  = 26;
-soundpack_index_mapping["16"]    = 27;
+soundpack_index_mapping["10"]    = 10;
+soundpack_index_mapping["10.5"]  = 10.5;
+soundpack_index_mapping["11"]    = 11;
+soundpack_index_mapping["11.5"]  = 11.5;
+soundpack_index_mapping["12"]    = 12;
+soundpack_index_mapping["13"]    = 13;
+soundpack_index_mapping["13.5"]  = 13.5;
+soundpack_index_mapping["14"]    = 14;
+soundpack_index_mapping["14.5"]  = 14.5;
+soundpack_index_mapping["15"]    = 15;
+soundpack_index_mapping["15.5"]  = 15.5;
+soundpack_index_mapping["16"]    = 16;
 
 soundpack_index_mapping["17"]   = 28;
 soundpack_index_mapping["17.5"] = 29;
@@ -51,76 +50,78 @@ soundpack_index_mapping["22"]   = 37;
 soundpack_index_mapping["22.5"] = 38;
 soundpack_index_mapping["23"]   = 39;
 
-soundpack_index_mapping["24"]   = 40;
-soundpack_index_mapping["24.5"] = 41;
-soundpack_index_mapping["25"]   = 42;
-soundpack_index_mapping["25.5"] = 43;
-soundpack_index_mapping["26"]   = 44;
-soundpack_index_mapping["27"]   = 45;
-soundpack_index_mapping["27.5"] = 46;
-soundpack_index_mapping["28"]   = 47;
-soundpack_index_mapping["28.5"] = 48;
-soundpack_index_mapping["29"]   = 49;
-soundpack_index_mapping["29.5"] = 50;
-soundpack_index_mapping["30"]   = 51;
+soundpack_index_mapping["24"]   = 24;
+soundpack_index_mapping["24.5"] = 24.5;
+soundpack_index_mapping["25"]   = 25;
+soundpack_index_mapping["25.5"] = 25.5;
+soundpack_index_mapping["26"]   = 26;
+soundpack_index_mapping["27"]   = 27;
+soundpack_index_mapping["27.5"] = 27.5;
+soundpack_index_mapping["28"]   = 28;
+soundpack_index_mapping["28.5"] = 28.5;
+soundpack_index_mapping["29"]   = 29;
+soundpack_index_mapping["29.5"] = 29.5;
+soundpack_index_mapping["30"]   = 30;
 
-soundpack_index_mapping["31"]   = 52;
-soundpack_index_mapping["31.5"] = 53;
-soundpack_index_mapping["32"]   = 54;
-soundpack_index_mapping["32.5"] = 55;
-soundpack_index_mapping["33"]   = 56;
-soundpack_index_mapping["34"]   = 57;
-soundpack_index_mapping["34.5"] = 58;
-soundpack_index_mapping["35"]   = 59;
-soundpack_index_mapping["35.5"] = 60;
-soundpack_index_mapping["36"]   = 61;
-soundpack_index_mapping["36.5"] = 62;
-soundpack_index_mapping["37"]   = 63;
+soundpack_index_mapping["31"]   = 31;
+soundpack_index_mapping["31.5"] = 31.5;
+soundpack_index_mapping["32"]   = 32;
+soundpack_index_mapping["32.5"] = 32.5;
+soundpack_index_mapping["33"]   = 33;
+soundpack_index_mapping["34"]   = 34;
+soundpack_index_mapping["34.5"] = 34.5;
+soundpack_index_mapping["35"]   = 35;
+soundpack_index_mapping["35.5"] = 35.5;
+soundpack_index_mapping["36"]   = 36;
+soundpack_index_mapping["36.5"] = 36.5;
+soundpack_index_mapping["37"]   = 37;
 
-soundpack_index_mapping["38"]   = 64;
-soundpack_index_mapping["38.5"] = 65;
-soundpack_index_mapping["39"]   = 66;
-soundpack_index_mapping["39.5"] = 67;
-soundpack_index_mapping["40"]   = 68;
-soundpack_index_mapping["41"]   = 69;
-soundpack_index_mapping["41.5"] = 70;
-soundpack_index_mapping["42"]   = 71;
-soundpack_index_mapping["42.5"] = 72;
-soundpack_index_mapping["43"]   = 73;
-soundpack_index_mapping["43.5"] = 74;
-soundpack_index_mapping["44"]   = 75;
+soundpack_index_mapping["38"]   = 38;
+soundpack_index_mapping["38.5"] = 38.5;
+soundpack_index_mapping["39"]   = 39;
+soundpack_index_mapping["39.5"] = 39.5;
+soundpack_index_mapping["40"]   = 40;
+soundpack_index_mapping["41"]   = 41;
+soundpack_index_mapping["41.5"] = 41.5;
+soundpack_index_mapping["42"]   = 42;
+soundpack_index_mapping["42.5"] = 42.5;
+soundpack_index_mapping["43"]   = 43;
+soundpack_index_mapping["43.5"] = 43.5;
+soundpack_index_mapping["44"]   = 44;
 
-soundpack_index_mapping["45"]   = 76;
-soundpack_index_mapping["45.5"] = 77;
-soundpack_index_mapping["46"]   = 78;
-soundpack_index_mapping["46.5"] = 79;
-soundpack_index_mapping["47"]   = 80;
-soundpack_index_mapping["48"]   = 81;
-soundpack_index_mapping["48.5"] = 82;
-soundpack_index_mapping["49"]   = 83;
-soundpack_index_mapping["49.5"] = 84;
-soundpack_index_mapping["50"]   = 85;
-soundpack_index_mapping["50.5"] = 86;
-soundpack_index_mapping["51"]   = 87;
+soundpack_index_mapping["45"]   = 45;
+soundpack_index_mapping["45.5"] = 45.5;
+soundpack_index_mapping["46"]   = 46;
+soundpack_index_mapping["46.5"] = 46.5;
+soundpack_index_mapping["47"]   = 47;
+soundpack_index_mapping["48"]   = 48;
+soundpack_index_mapping["48.5"] = 48.5;
+soundpack_index_mapping["49"]   = 49;
+soundpack_index_mapping["49.5"] = 49.5;
+soundpack_index_mapping["50"]   = 50;
+soundpack_index_mapping["50.5"] = 50.5;
+soundpack_index_mapping["51"]   = 51;
 
-soundpack_index_mapping["52"]   = 88;
+soundpack_index_mapping["52"]   = 52;
 
-//抓到audio中data-num=id的那個聲音DOM物件
+//推一包一包的資料進去，這邊會提供audio作渲染
 for(var i=1; i<=52; i+=0.5){
-  soundpack_index[i] = i;
-  var s = "./pianosound_mp3/"+ i + ".mp3";
-  soundpack.push({
-    number: i,
-    url: s,
-  });
+  // if(i!=2.5&&i!=5.5&&i!=9.5){
+    soundpack_index[i] = i;
+    var s = "./pianosound_mp3/"+ i + ".mp3";
+    soundpack.push({
+      number: i,
+      url: s,
+    });
+  }
   // console.log(s);
-}
+// }
 
 var vm = new Vue({
   el: "#app",
   data: {
     sounddata: soundpack,
-    notes: [{"num":24,"time":343},{"num":1,"time":265},{"num":5,"time":380},{"num":5,"time":501},{"num":6,"time":625},{"num":6,"time":748},{"num":5,"time":871},{"num":4,"time":1126},{"num":4,"time":1247},{"num":3,"time":1365},{"num":3,"time":1477},{"num":2,"time":1597},{"num":2,"time":1714},{"num":1,"time":1837}],
+    notes: [{"num":24,"time":150},{"num":24,"time":265},{"num":28,"time":380},{"num":28,"time":501},{"num":29,"time":625},{"num":29,"time":748},{"num":28,"time":871},{"num":27,"time":1126},{"num":27,"time":1247},{"num":26,"time":1365},{"num":26,"time":1477},{"num":25,"time":1597},{"num":25,"time":1714},{"num":24,"time":1837}],
 
     now_note_id: 0,//播放到哪裡
     next_note_id: 0,
@@ -229,20 +230,19 @@ var vm = new Vue({
     ]
   },
   methods: {
-    //播放音符，附上id音符號碼|Volume（0-1）音量
-    playnote: function(id,volume){
+    playnote: function(id,volume){//播放音符，附上id音符號碼|Volume（0-1）音量
       if (id>0){
-				var audio_obj=$("audio[data-num='"+id+"']")[0];
+				var audio_obj=$("audio[data-num='"+id+"']")[0];//抓到audio中data-num=id的那個聲音DOM物件
         audio_obj.volume=volume;//調整音量
         audio_obj.currentTime=0;//倒帶到頭
         audio_obj.play();//播放音樂
       }
     },
     playnext: function(volume){
-      //從notes裡面抓出第now_note_id筆資料
-      var play_note=this.notes[this.now_note_id].num;
+      
+      var play_note=this.notes[this.now_note_id].num;//從notes裡面抓出第now_note_id筆資料
       // console.log('play_note = ' + play_note);
-			play_note = soundpack_index_mapping[play_note.toString()];
+      play_note = soundpack_index_mapping[play_note.toString()];
       this.playnote(play_note,volume);//播放音符（引數音符號碼、音量）
 			this.now_note_id+=1;//把現在正在播放的音符位置移動到下一個
 
@@ -261,10 +261,8 @@ var vm = new Vue({
     },
     //停止錄音
     stop_record: function(){
-      //清除計時器
-      clearInterval(this.recorder);
-      //重置錄製時間
-      this.record_time=0;
+      clearInterval(this.recorder);//清除計時器
+      this.record_time=0;//重置錄製時間
       
     },
     //開始播放
@@ -275,20 +273,17 @@ var vm = new Vue({
       var vobj=this;//用與在setInterval能夠存取this，運用vobj當變數裝他
       //播放的計時器
       this.player=setInterval(function(){
-        //如果現在播放時間>下一個音符的時間的話
-        if (vobj.playing_time>=vobj.notes[vobj.next_note_id].time){
-          //播放下一個音符，下一個音符的index+=1
-          vobj.playnext(1);
+        if (vobj.playing_time>=vobj.notes[vobj.next_note_id].time){//如果現在播放時間>下一個音符的時間的話
+          vobj.playnext(1); //播放下一個音符，下一個音符的index+=1
           vobj.next_note_id++;
+          
         }
-        //播放時間+1
-        vobj.playing_time++;
+        vobj.playing_time++;//播放時間+1
       },2);
     },
     //結束播放
     stopplay: function(){
-      //清除正在驅動的player計時器
-      clearInterval(this.player);
+      clearInterval(this.player);//清除正在驅動的player計時器
       this.now_note_id=0;//現在指向的音符位置為0 
       this.next_note_id=0;//歸零現在播放時間
       this.playing_time=0;//下一個音符為0
@@ -303,40 +298,28 @@ var vm = new Vue({
         return true;
       if (this.notes.length==0)
         return false
-      //cur-id上一個播放的音符ID
-      var cur_id=this.now_note_id-1;
-      //如果cur-id《0會發生錯誤，歸零
-      if (cur_id<0) cur_id=0;
-      //取得現在的播放音符
-      var num=this.notes[cur_id].num;
+      
+      var cur_id=this.now_note_id-1;//cur-id上一個播放的音符ID
+      if (cur_id<0) cur_id=0;//如果cur-id<0會發生錯誤，歸零
+      var num=this.notes[cur_id].num; //取得現在的播放音符
       //如果播放與傳進來的音符一樣，傳true，否則執行到最後回傳false
-			
 			//顯示播放音符位置
-			//error
-			if ((num)==id){
-				// console.log('num = '+(num) +' id = '+(id));
+			if ((num)==id)
 				return true;
-			}
 			return false;
-			//error
-
-
     },
     //加入音符到樂譜（如果現在正在錄製中），播放
     addnote: function(id){
-      //如果正在錄製中(錄製時間>0)，就推一個音符資料(音符號碼/播放時間)進去樂譜
-      // console.log('id = ' + id);
-
-      if (this.record_time>0)
+      console.log('id = ' + id);
+      if (this.record_time>0) //如果正在錄製中(錄製時間>0)，就推一個音符資料(音符號碼/播放時間)進去樂譜
         this.notes.push({num: id,time: this.record_time});
-         //播放這音樂
-      this.playnote(id,1);
+      this.playnote(id,1);//播放這音樂
     },
     load_sample: function(){
       var vobj=this;
       $.ajax({
         url: "https://awiclass.monoame.com/api/command.php?type=get&name=music_dodoro",
-        //url: "./music_dodoro.json",
+        // url: "./music_dodoro.json",
         success: function(res){
           vobj.notes=JSON.parse(res);
         }
